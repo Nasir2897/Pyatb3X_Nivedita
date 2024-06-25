2d062d448bcacdf24fcd1c60ae7ce37615095349
Nasir2897 <shaikhnasir371@gmail.com>
2024-06-25 10:51:12 +0000

    All files

---
+++ 10_24Jun24/Lab101.py
@@ -0,0 +1,14 @@
+#Dictionary
+
+my_dict = {
+    'b' : 2,
+    'a' : 1,
+    'c' : 3
+}
+
+for k,v in my_dict.items():
+    if k == 'b':
+        print("key with the name b is found")
+
+op = 'b' in my_dict
+print(op)
